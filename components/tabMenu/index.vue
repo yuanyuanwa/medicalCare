38221b77fc8c58a2b89f1a79e7aea84710f08a2a
v-liyy84 <1820001685@qq.com>
2023-01-06 16:23:58 +0800

    新增readme

--- components/tabMenu/index.vue
+++ components/tabMenu/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<view>
-		<view class="flex-around pb-40 tab-box">
+		<view class="flex-around pb-20 tab-box">
 			<!-- 	<view class="activeBar p-10">
 				今日
 			</view>
@@ -16,8 +16,10 @@
 			</view> -->
 			<view class="tab-item" @click="testTabClick(index)" :class="tabIndex == index?'active':''"
 				v-for="(item,index) in tabList" :key="index">
-				{{item.name}}
-				<uni-icons v-if="item.name=='筛选日期'" type="bottom" size="20" color="white"></uni-icons>
+				<view class="p-10">{{item.name}}
+					<uni-icons v-if="item.name=='筛选日期'" type="bottom" size="20" color="white"></uni-icons>
+				</view>
+
 			</view>
 		</view>
 	</view>
@@ -40,7 +42,7 @@
 			}
 		},
 		methods: {
-testTabClick(index) {
+			testTabClick(index) {
 				console.log(index)
 				this.tabIndex = index
 			}
@@ -49,28 +51,31 @@ testTabClick(index) {
 </script>
 
 <style lang="scss" scoped>
-	.tab-box{
-			display: flex;
-			.tab-item{
-				flex-shrink: 0;
-				padding: 24rpx;
-				position: relative;
-				&::after{
-					transition: all 0.2s linear;
-					transform: translateX(-50%) scaleX(0);
-					content: '';
-					width: 60%;
-					position: absolute;
-					left: 50%;
-					bottom: 20rpx;
-					border-bottom: 6rpx solid white;
-					border-radius: 4rpx;
-				}
-				&.active{
-					&::after{
-						transform: translateX(-50%) scaleX(1);					
-					}
+	.tab-box {
+		display: flex;
+
+		.tab-item {
+			flex-shrink: 0;
+			padding: 24rpx;
+			position: relative;
+
+			&::after {
+				transition: all 0.2s linear;
+				transform: translateX(-50%) scaleX(0);
+				content: '';
+				width: 60%;
+				position: absolute;
+				left: 50%;
+				bottom: 20rpx;
+				border-bottom: 6rpx solid white;
+				border-radius: 4rpx;
+			}
+
+			&.active {
+				&::after {
+					transform: translateX(-50%) scaleX(1);
 				}
 			}
 		}
+	}
 </style>

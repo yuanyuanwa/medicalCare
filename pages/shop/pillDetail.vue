aa5be9063058629507201f5e91e579faca42b084
v-liyy84 <1820001685@qq.com>
2023-01-10 17:36:51 +0800

    add

--- pages/shop/pillDetail.vue
+++ pages/shop/pillDetail.vue
@@ -15,8 +15,60 @@
 			</view>
 		</view>
 		<view class="card flex-between" style="padding: 20rpx 20rpx;">
-			<view >医生诊断与处方</view>
-			<view class="flex-align-center">查看详情<uni-icons type="forward" size="30"></uni-icons></view>
+			<view>医生诊断与处方</view>
+			<view class="flex-align-center" @click="toDetail">查看详情<uni-icons type="forward" size="30"></uni-icons>
+			</view>
+		</view>
+		<view class="card">
+			<view class="pt-20 pb-40 font-20 pl-10" style="border-bottom: 1px solid gainsboro;">药品信息</view>
+			<view class="p-20">
+				<view class="font-bold pt-20 pb-20">
+					盐酸氟桂胶囊
+				</view>
+				<view style="color: #888989;" class="pb-40">XXXX生物制药公司</view>
+				<view class="flex-between pb-20">
+					<view style="color: #888989;">5mg*24粒</view>
+					<view>￥14</view>
+				</view>
+				<view class="flex-between pb-20">
+					<view style="color: #888989;">发货不支持退换</view>
+				</view>
+				<view class="flex-end pb-20" style="border-bottom: 1px solid gainsboro;">
+					<view>x1</view>
+				</view>
+				<view class="flex-between pb-20 pt-20">
+					<view style="color: #888989;">商品总金额</view>
+					<view>￥14</view>
+				</view>
+				<view class="flex-between pb-40">
+					<view style="color: #888989;">配送费</view>
+					<view>￥8</view>
+				</view>
+				<view class="flex-between pb-20 font-20 font-bold">
+					<view>实付总额</view>
+					<view style="color: #D43030;">￥22</view>
+				</view>
+			</view>
+		</view>
+		<view class="card">
+			<view class="pt-20 pb-20 font-20 pl-10">订单信息</view>
+			<view class="p-20">
+				<view class="flex-between pb-20">
+					<view style="color: #888989;">订单编号：12312312</view>
+					<view class="copy">复制</view>
+				</view>
+				<view class="flex-between pb-20">
+					<view style="color: #888989;">下单时间：2023-01-10 15：10：19</view>
+				</view>
+				<view class="flex-between pb-20">
+					<view style="color: #888989;">配送方式：送药上门</view>
+				</view>
+			</view>
+		</view>
+		<view class="card flex-end" style="padding: 30rpx;">
+			<view class="check">
+				查看物流
+			</view>
 		</view>
 	</view>
 </template>
@@ -33,7 +85,12 @@
 			}
 		},
 		methods: {
-
+			toDetail() {
+				uni.navigateTo({
+					//关闭当前页面，跳转到应用内的某个页面。
+					url: '/pages/shop/prescription'
+				});
+			}
 		}
 	}
 </script>
@@ -41,7 +98,7 @@
 <style scoped lang="scss">
 	.box {
 		background: #F6F8F7;
-		height: 95vh;
+		height: 100%;
 	}
 
 	.card {
@@ -50,5 +107,20 @@
 		font-size: 18px;
 		margin: 20rpx;
 		padding: 0rpx 20rpx;
+	}
+
+	.copy {
+		border: 1px solid #5F99FC;
+		padding: 0rpx 30rpx;
+		border-radius: 100rpx;
+		color: #5F99FC;
+	}
+
+	.check {
+		padding: 20rpx 40rpx;
+		border-radius: 100rpx;
+		color: #5F99FC;
+		background: #EFF7FF;
+		font-weight: bold;
 	}
 </style>
---
+++ pages/shop/prescription.vue
@@ -0,0 +1,41 @@
+<template>
+	<view class="box">
+		<commonHeader :title="title" :hasBack='true' :bgcolor="'#5878FC'" :color="'white'" />
+		<view class="flex-center">
+			<view class="card"></view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import commonHeader from '@/components/header/common.vue'
+	export default {
+		components: {
+			commonHeader,
+		},
+		data() {
+			return {
+				title: 'XXX医生'
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.box {
+		height: 100vh;
+		background: #F0F0FA;
+	}
+
+	.card {
+		height: 300rpx;
+		width: 90%;
+		background: #fff;
+		margin-top: 40rpx;
+		border-radius: 20rpx;
+		box-shadow: 0px 2px 12px 0px #E4E4ED;
+	}
+</style>
